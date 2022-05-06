64e8a28a87c087d14028e0f41ba9000adbe5e2ba
Simon Cain <rudenik@gmail.com>
2022-05-06 10:39:34 -0400

    updated packages and removed some option for mongoose

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ const scheduler = require("./scheduler")
 var MONGODB_URI = process.env.MONGODB_URI
 
 mongoose.Promise = Promise;
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useNewUrlParser', true );
+// mongoose.set('useCreateIndex', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useUnifiedTopology', true);
+// mongoose.set('useNewUrlParser', true );
 
 const mongoOptions = {
     useNewUrlParser: true,
